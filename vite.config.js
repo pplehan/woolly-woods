13e985d50fed1bec48a92a6270a2a0b9da7e868a
Raven <149933965+pplehan@users.noreply.github.com>
2025-02-16 18:18:05 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: "/woolly-woods/"
+  base: "/woolly-woods/",
   resolve: {
     alias: { // 2. 新增別名
     "@": path.resolve(__dirname, "./src"),
