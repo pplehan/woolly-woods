93cee0bcb7df8598ca8916a79b30966f4232457d
pplehan <csp31916@gmail.com>
2025-02-16 17:41:57 +0800

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:'./',
   plugins: [vue()],
   resolve: {
     alias: { // 2. 新增別名
