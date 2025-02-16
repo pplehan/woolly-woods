e0db5c323d6f60bd69e84f7c58a63ac36f964315
pplehan <csp31916@gmail.com>
2025-02-16 17:58:49 +0800

    feat: change pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import vue from '@vitejs/plugin-vue'
 import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'./',
   plugins: [vue()],
   resolve: {
     alias: { // 2. 新增別名
