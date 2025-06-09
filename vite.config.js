c35482483cf65febf4144dd7404eed3b6c49ab5e
pplehan <csp31916@gmail.com>
2025-06-09 10:03:16 +0800

    Fix vite.config.js conflict

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [vue()],
-<<<<<<< HEAD
-  base: "/woolly-woods/",
-=======
   base: '/woolly-woods/',
->>>>>>> 22b0264 (Fix resource paths for GitHub Pages)
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
